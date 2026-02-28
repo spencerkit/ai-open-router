5130fffce82255067412889e1113ee1684588739
Spencer Ye <pallyoung@163.com>
2026-03-01 04:14:35 +0800

    config: add Vite configuration

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+export default defineConfig({
+  base: './',
+  root: 'src/renderer',
+  plugins: [react()],
+  build: {
+    outDir: '../../out/renderer',
+    emptyOutDir: true,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src/renderer'),
+    },
+  },
+});
